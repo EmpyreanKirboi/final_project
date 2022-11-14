d2f7f6c2af5958d8a632c71e959b7474123dd4c5
EmpyreanKirboi <106829902+EmpyreanKirboi@users.noreply.github.com>
2022-11-14 13:43:56 -0500

    update hw part 2

--- newgame.py
+++ newgame.py
@@ -7,22 +7,6 @@ import sys
 screen = pygame.display.set_mode((400, 400))
 screen.fill((0, 0, 255))
 
-while True:
-    print("----------check for new events____________")
-    recent_events = pygame.event.get()
-    print("----------done checking for events--------")
-    for event in recent_events:
-        if event.type == pygame.KEYDOWN:
-            print("I will not quit")
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_r:
-                screen.fill((255, 0, 0))
-            elif event.key == pygame.K_g:
-                screen.fill((0, 255, 0))
-            elif event.key == pygame.K_b:
-                screen.fill((0, 0, 255))
 
     pygame.display.flip()
     time.sleep(1)
--- settings.py
+++ settings.py
@@ -1,20 +1,26 @@
 class Settings:
-    """A class to store all settings for Alien Invasion"""
+    """A class to store all settings for Sideways Shooter."""
 
     def __init__(self):
         """Initialize the game's settings."""
-        # Screen Settings
+        # Screen settings
         self.screen_width = 1200
-        self.screen_height = 600
-        self.bg_image = (40, 20, 40)
+        self.screen_height = 800
+        self.bg_color = (0, 230, 230)
 
+        # Ship settings
+        self.ship_speed = 1.0
+        self.ship_limit = 3
 
-        # Ship Settings
-        self.ship_speed = 0.8
+        # Bullet settings
+        self.bullet_speed = 6.0
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = (0, 60, 60)
+        self.bullets_allowed = 3
 
-        # Bullet Settings
-        self.bullet_speed = 1.0
-        self.bullet_width = 3
-        self.bullet_height = 15
-        self.bullet_color = (200, 0, 0)
-        self.bullets_allowed = 20
+        # Alien settings.
+        #  alien_frequency controls how often a new alien appear.s
+        #    Higher values -> more frequent aliens. Max = 1.0.
+        self.alien_frequency = 0.015
+        self.alien_speed = 0.3
