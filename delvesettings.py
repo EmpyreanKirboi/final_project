adcf927304b6725465cc8b77b3bb97d6d02045da
EmpyreanKirboi <106829902+EmpyreanKirboi@users.noreply.github.com>
2022-11-27 14:42:30 -0500

    update for screen

---
+++ delvesettings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A class to store settings"""
+
+    def __init__(self):
+        # Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 600
+        self.bg_color = (0, 0, 60)
